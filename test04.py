921140c98df0eb658e232838e2c1e3a68077aa96
ratchata6419410037 <s6419410037@sau.ac.th>
2023-09-22 10:25:07 +0700

    code v1 : add test-01-06

---
+++ test04.py
@@ -0,0 +1,14 @@
+#if-else-if พิสูจน์ / ตรวจสอบ หลายครั้ง พิสูจน์ทีละ if
+
+if 50 > 60 :
+    print('Hi....')
+elif 'Sombat' == 'Somjai' :
+    print('Hello..')
+elif 100 < 80 :
+    print('Hey...')        
+    print('Hoo...') 
+else :
+    print('Iot...')       
+    print('Sau...')       
+print('wow...')       
+print('woo...')       
---
+++ test05.py
@@ -0,0 +1,10 @@
+# while
+
+x = 1
+
+while x <= 10 :
+    print('Iot')
+    x = x + 2
+
+    print('SAU')
+    print(f'x => {x}')
