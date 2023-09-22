31c1cbe743bdaf7b56ab6e609b5f179bbe0e1bdd
ratchata6419410037 <142560668+ratchata6419410037@users.noreply.github.com>
2023-09-22 10:53:49 +0700

    Update test06.py
    add text in test06

--- test06.py
+++ test06.py
@@ -2,7 +2,20 @@
 
 for x in range(5) :
     print('IoT')
-
 print('++++++++++++++++++++')
 for y in range(10) :
-    print('SAU')    
+    print('SAU {y}')
+print('++++++++++++++++++++')
+
+for z in range(2, 10) : #เริ่มที่ 2 - 9
+    print(f'Hi {z}')
+
+print('+++++++++++++')
+
+for a in range(3, 10, 2) : # เริ่มที่ 3 - 9 / step เป็น
+    print(f'Hey {a}')
+
+print('+++++++++++++++++++++')
+
+for b in 'Welcome to thailand' :
+    print(f'Hoo {b}')
